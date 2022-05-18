6a4238c56393be4f819adb130bedfa8ee35bfdf4
JustEp1c <timofey_vdv@mail.ru>
2022-05-18 13:58:54 +0300

    Basic requests to the server (registration and login)

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from flask import render_template
-
+from flask import render_template, request, redirect, url_for
+import requests
+import json
 
 app = Flask(__name__)
+
+register_url = "https://best-routes.herokuapp.com/user/register"
+login_url = "https://best-routes.herokuapp.com/user/login"
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,9 +14,36 @@ def home_page():
     return render_template('index.html')
 
 
-@app.route('/auth')
+@app.route('/auth', methods=['GET', 'POST'])
 def auth_page():
-    return render_template('auth.html')
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        headers = {"Content-Type": "application/json"}
+        payload = json.dumps({"email": email, "password": password})
+
+        response = requests.request("POST", login_url, headers=headers, data=payload)
+        print(response.text)
+        return redirect('/')
+    else:
+        return render_template('auth.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        headers = {"Content-Type": "application/json"}
+        payload = json.dumps({"email": email, "password": password})
+
+        response = requests.request("POST", register_url, headers=headers, data=payload)
+        print(response.text)
+        return redirect('/')
+    else:
+        return render_template('register.html')
 
 
 if __name__ == '__main__':
